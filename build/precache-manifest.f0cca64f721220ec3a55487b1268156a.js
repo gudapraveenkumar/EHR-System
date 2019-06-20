01022f208fffd2b2ff3333818ae6e3cce29c16e4
Praveen Kumar Guda <praveenguda.kumar@gmail.com>
2019-06-20 23:27:41 +0200

    Change dev dependencies

---
+++ build/precache-manifest.f0cca64f721220ec3a55487b1268156a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751162eeccbea1746ae7e8d186f6adc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68814345df2c997d9322",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "27bed8d0f76cab2e7012",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "68814345df2c997d9322",
+    "url": "/static/js/2.06a88a12.chunk.js"
+  },
+  {
+    "revision": "27bed8d0f76cab2e7012",
+    "url": "/static/js/main.904dc8b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
