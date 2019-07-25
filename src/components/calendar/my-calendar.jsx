072c137f8bad4db01e1d6605ba1ad4810a8d4772
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-07-25 18:54:46 +0200

    Add calendar

--- src/components/calendar/my-calendar.jsx
+++ src/components/calendar/my-calendar.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './calendar.scss';
+
+const localizer = momentLocalizer(moment)
 
 class MyCalendar extends Component {
-   state = {  }
+   state = {
+      events:[
+         {
+            title: 'Finish',
+            start: new Date(),
+            end: new Date('2019-07-26'),
+            allDay: false
+          },
+          {
+            title: 'test app',
+            start: new Date(),
+            end: new Date('2019-07-26'),
+            allDay: false
+          }
+      ]
+    }
    render() {
-      return (
-         <div>
-            My Calendar
+      return(
+         <div style={{height: "80vh", padding:'20px'}} className="calendar-container">
+            <h2 style={{textAlign:"center"}}>My Calendar</h2>
+           <Calendar style={{marginTop:'30px'}}
+             localizer={localizer}
+             events={this.state.events}
+             startAccessor="start"
+             endAccessor="end"
+            />
          </div>
-       );
+       )
    }
 }
 
