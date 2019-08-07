25e2a21d3caace845105760b4018b304c14234ab
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-08-07 23:50:47 +0200

    Add branch name using colon

--- src/components/auth/registration.jsx
+++ src/components/auth/registration.jsx
@@ -50,7 +50,7 @@ class Registration extends Component {
                <Col xs={10} sm={8} md={5}>
                <Card>
                      <Card.Body>
-                        <Card.Title className="text-center">Register / Signup/ Debba Raju</Card.Title>
+                        <Card.Title className="text-center">Register / Signup/ Debba </Card.Title>
                         <br></br>
                         <div>
                            <Form onSubmit={this.handleRegisterSubmit}>
