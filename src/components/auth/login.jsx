c61a5f885f88f670d59215cc63d5355f9a01bf6c
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-08-07 22:55:50 +0200

    Test push to check merging in travis

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -51,7 +51,7 @@ class Login extends Component {
                   <br></br>
                   <Card>
                      <Card.Body>
-                        <Card.Title className="text-center">Login / SignIn</Card.Title>
+                        <Card.Title className="text-center">Login / SignIn / Register Debba</Card.Title>
                            <Form onSubmit={this.handleLoginSubmit}>
                               <Form.Group controlId="formBasicEmail">
                                  <Form.Label>Username</Form.Label>
