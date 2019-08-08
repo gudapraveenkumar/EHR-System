d0567fe7fe599f5dd8f4b02f8d2de8ec1fba9f8b
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-08-08 10:16:20 +0200

    change origin command

--- merge_script.sh
+++ merge_script.sh
@@ -14,11 +14,11 @@ commit_website_files() {
 upload_files() {
    git remote -v
    git remote remove origin
-   git remote add origin git://github.com/gudapraveenkumar/task-manager.git
+   git remote add origin git@github.com:gudapraveenkumar/task-manager.git
    git push https://${GITHUB_KEY}@github.com/gudapraveenkumar/task-manager.git
 
    # git push https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/gudapraveenkumar/task-manager.git
-   # # git remote add origin-pages https://${GITHUB_KEY}@github.com/gudapraveenkumar/task-manager.git > /dev/null 2>&1
+   # git remote add origin-pages https://${GITHUB_KEY}@github.com/gudapraveenkumar/task-manager.git > /dev/null 2>&1
    # git push --quiet origin master
 }
 
