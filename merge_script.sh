25816c4439507ce18522243a4302ad0619f77fbd
gudapraveenkumar <praveenguda.kumar@gmail.com>
2019-08-08 17:45:12 +0200

    Replace upload files block

--- merge_script.sh
+++ merge_script.sh
@@ -12,15 +12,23 @@ commit_website_files() {
 }
 
 upload_files() {
-   git remote -v
-   git remote remove origin
-   # git remote add origin git@github.com:gudapraveenkumar/task-manager.git
-   # git push https://${GITHUB_KEY}@github.com/gudapraveenkumar/task-manager.git
-
-   # git push https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/gudapraveenkumar/task-manager.git
-   git remote add travis-user1 https://9ac4fe5751919002e3d34103578db6ec4104bcd1@github.com/gudapraveenkumar/task-manager.git
-   git push travis-user1 master
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://gudapraveenkumar:${GH_TOKEN}@github.com/gudapraveenkumar/task-manager.git > /dev/null 2>&1
+  git push origin master --quiet
 }
+
+# upload_files() {
+#    git remote -v
+#    git remote remove origin
+#    # git remote add origin git@github.com:gudapraveenkumar/task-manager.git
+#    # git push https://${GITHUB_KEY}@github.com/gudapraveenkumar/task-manager.git
+
+#    # git push https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/gudapraveenkumar/task-manager.git
+#    git remote add travis-user1 https://@github.com/gudapraveenkumar/task-manager.git
+#    git push travis-user1 master
+# }
 
 setup_git
 commit_website_files
